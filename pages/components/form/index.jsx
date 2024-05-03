f518dd93dad0e355f4448475c953f720ac58e317
jonathan-at-230349630805 <jonathancosta1717@gmail.com>
2024-05-03 10:16:44 -0300

    Commit fix

--- pages/components/form/index.jsx
+++ pages/components/form/index.jsx
@@ -6,7 +6,7 @@ export default function GenericForm({fields}){
     return(
         <Form className='Form'>
             <div className='container'>
-                {fields.map((item) => (
+                {fields?.map((item) => (
                     <Form.Group className='form-group'>
                         <Form.Label className='label'>{`${item.label}: `}</Form.Label>
                         <Form.Control className={item.style} type={item.type} prefix={item.prefix? item.prefix : null} placeholder={`insira o ${item.label}`} />
