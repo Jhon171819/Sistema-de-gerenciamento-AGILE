28b8e8301b79c4171e444596023eb65267a14ef0
jonathan-at-230349630805 <jonathancosta1717@gmail.com>
2024-05-13 15:16:19 -0300

    sideBarFix

--- pages/components/sideBar.tsx
+++ pages/components/sideBar.tsx
@@ -12,61 +12,53 @@ interface SideBarProps {
 
 export default function SideBar(): JSX.Element {
   return (
-    <div>
-        <div style={divStyle}>
-          <a
-            href="https://github.com/Jhon171819"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-             <span>Github &rarr;</span>
-          </a>
-        </div>
-        <div style={divStyle}>
-          <a href="/cadastroProdutos/" className={styles.card}>
+    <><div>
+      <div style={divStyle}>
+        <a
+          href="https://github.com/Jhon171819"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span>Github &rarr;</span>
+        </a>
+      </div>
+      <div style={divStyle}>
+        <a href="/cadastroProdutos/" className={styles.card}>
           <span>Cadastro de produto &rarr;</span>
-          </a>
-        </div>
-        <div style={divStyle}>
-          <a href="/cadastroFornecedores/" className={styles.card}>
-             <span>Cadastro de fornecedor &rarr;</span>
-          </a>
-        </div>
-        <div style={divStyle}>
-<<<<<<< HEAD
-          <a href='/storeManagement/' className={styles.card}>
-            <span>Administração do banco &rarr;</span>
-          </a>
-        </div>
-=======
-          <a href="/cadastroClientes/" className={styles.card}>
-             <span>Cadastro de Clientes &rarr;</span>
-          </a>
-        </div>
-        <div style={divStyle}>
-          <a href="/cadastroCompras/" className={styles.card}>
-             <span>Cadastro de Compras &rarr;</span>
-          </a>
-        </div>
-        <div style={divStyle}>
-          <a href="/cadastroItemCompras/" className={styles.card}>
-             <span>Cadastro de Item de Compras &rarr;</span>
-          </a>
-        </div>
-        <div style={divStyle}>
-          <a href="/cadastroVendas/" className={styles.card}>
-             <span>Cadastro de Vendas &rarr;</span>
-          </a>
-        </div>
-        <div style={divStyle}>
-          <a href="/cadastroItemVendas/" className={styles.card}>
-             <span>Cadastro de Item de Vendas &rarr;</span>
-          </a>
-        </div>
-        
->>>>>>> FrontEndTelas
-    </div>
+        </a>
+      </div>
+      <div style={divStyle}>
+        <a href="/cadastroFornecedores/" className={styles.card}>
+          <span>Cadastro de fornecedor &rarr;</span>
+        </a>
+      </div>
+      <div style={divStyle}>
+        <a href='/storeManagement/' className={styles.card}>
+          <span>Administração do banco &rarr;</span>
+        </a>
+      </div>
+      <a href="/cadastroClientes/" className={styles.card}>
+        <span>Cadastro de Clientes &rarr;</span>
+      </a>
+    </div><div style={divStyle}>
+        <a href="/cadastroCompras/" className={styles.card}>
+          <span>Cadastro de Compras &rarr;</span>
+        </a>
+      </div><div style={divStyle}>
+        <a href="/cadastroItemCompras/" className={styles.card}>
+          <span>Cadastro de Item de Compras &rarr;</span>
+        </a>
+      </div><div style={divStyle}>
+        <a href="/cadastroVendas/" className={styles.card}>
+          <span>Cadastro de Vendas &rarr;</span>
+        </a>
+      </div>
+      <div style={divStyle}>
+        <a href="/cadastroItemVendas/" className={styles.card}>
+          <span>Cadastro de Item de Vendas &rarr;</span>
+        </a>
+      </div></>
   );
 }
 
