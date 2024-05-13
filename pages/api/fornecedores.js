8b0f57d504dce80e802ef15204ffdfdb8ea36409
Renan Castro <renan9008@gmail.com>
2024-05-13 14:52:49 -0300

    fornecedores bigint

--- pages/api/fornecedores.js
+++ pages/api/fornecedores.js
@@ -6,8 +6,10 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
       const fornecedores = await prisma.fornecedor.findMany();
-      
-      res.status(200).json(fornecedores);
+      const serializedFornecedores = JSON.stringify(fornecedores, (key, value) =>
+        typeof value === 'bigint' ? value.toString() : value
+      );
+      res.status(200).json(JSON.parse(serializedFornecedores));
     } catch (error) {
       res.status(500).json({ message: 'Erro ao buscar fornecedores', error: error.message });
     }
@@ -16,7 +18,7 @@ export default async function handler(req, res) {
       const { id_fornecedor, nome_fornecedor, rua, bairro, cidade, estado, cep, email, telefone_celular } = req.body;
 
       const novoFornecedor = await prisma.fornecedor.create({
-        data: { id_fornecedor, nome_fornecedor, rua, bairro, cidade, estado, cep, email, telefone_celular }
+        data: { id_fornecedor, nome_fornecedor, rua, bairro, cidade, estado, cep, email, telefone_celular:BigInt(telefone_celular) }
       });
       res.status(201).json(novoFornecedor);
     } catch (error) {
